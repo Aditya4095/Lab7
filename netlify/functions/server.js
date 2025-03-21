306913f1a75f7eaeb7a3c9de4e2f0abcceee2420
Aditya4095 <ad963939@dal.ca>
2025-03-21 16:06:12 -0300

    lab7

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const serverless = require('serverless-http');
 const cors = require('cors');
 const axios = require('axios');
-const projects = require('../../src/projects.json'); // Adjust path based on where your JSON file is
+const projects = require('./../../projects.json');  
 
 
 
